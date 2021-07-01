95f9dd37add7e4e758d8e8ae366257a0dae896d7
Student <student@lan.itstep.kiev.ua>
2021-07-01 21:58:22 +0300

    calculator

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-print("hello phyton")
-a = 1+1
-print("=",a,)
+#print("hello phyton")
 
+num1 = float(input("firstint : "))
+num2 = float(input("secondint : "))
+symbol = input("+,-,%,*,/")
 
+if symbol == "-":
+    num = num1 - num2
+    print(num)
+elif symbol == "+":
+    num = num1 + num2
+    print(num)
+elif symbol == "*":
+    print(num1 * num2)
+elif symbol == "/":
+    print(num1 / num2)
+else:
+    print("im sorry")
 
 
 
